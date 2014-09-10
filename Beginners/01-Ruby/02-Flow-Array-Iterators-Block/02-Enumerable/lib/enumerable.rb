beee626e946812d9e82a91e6e0fbd72dc035dad9
Martin Mignot <martin.mignot@gmail.com>
2014-09-10 10:43:20 +0000

    enumerable exercises

--- Beginners/01-Ruby/02-Flow-Array-Iterators-Block/02-Enumerable/lib/enumerable.rb
+++ Beginners/01-Ruby/02-Flow-Array-Iterators-Block/02-Enumerable/lib/enumerable.rb
@@ -1,42 +1,43 @@
 array = [1,2,3,4,5,6,7,8,9,10]
+array_string = ["joe","marie","amandine","antoine","paul","pierre"]
 
 def sum_odd_indexed(array)
   array.values_at(*array.each_index.select{|i| i.odd?}).map{|i| i.to_i}.inject(&:+)
-    
-  # TODO: computes the sum of elements at odd indices (1, 3, 5, 7, etc.) 
-  #       You should make use Enumerable#each_with_index
 end
- 
 puts sum_odd_indexed(array)
 
+
 def even_numbers(array)
-  # TODO: Return the even numbers from a list of integers.
-  #       You should use Enumerable#select
+  array.select { |i|  i.even?  }
 end
+puts even_numbers(array)
 
-def short_words(array, max_length)
-  # TODO: Take and array of words, return the array of words not exceeding max_length characters
-  #       You should use Enumerable#reject
+
+max_length = 4
+def short_words(array_string, max_length)
+  array_string.reject { |i| i.length > max_length }
 end
- 
+puts short_words(array_string, max_length)
+
+limit = 3
 def first_under(array, limit)
-  # TODO: Return the first number from an array that is less than limit.
-  #       You should use Enumerable#find
+  array.detect  { |i| i < limit }
 end
- 
-def add_bang(array)
-  # TODO: Take an array of strings and return a new array with "!" appended to each string.
-  #       You should use Enumerable#map
-end
- 
-def product(array)
-  # TODO: Calculate the product of an array of numbers.
-  #       You should use of Enumerable#reduce
-end
- 
-def sorted_pairs(array)
-  # TODO: Reorganize an array into slices of 2 elements, and sort each slice alphabetically.
-  #       You should make use of Enumerable#each_slice
-end
+puts first_under(array, limit) 
 
+def add_bang(array_string)
+  array_string.map { |i| i + "!"}
+end
+puts add_bang(array_string)
+    
+def product(array)
+  array.reduce(:*) 
+end
+puts product(array)
+ 
+def sorted_pairs(array_string)
+  slice = [array_string.each_slice(2) { |i| p i }]
+  sort = slice.sort  
+end
+puts sorted_pairs(array_string).sort
 
