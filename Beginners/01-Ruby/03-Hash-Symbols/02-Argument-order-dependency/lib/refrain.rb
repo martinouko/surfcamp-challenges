48d9ea6486929c120ad8105e6171ee7f47afdab1
Martin Mignot <martin.mignot@gmail.com>
2014-09-11 08:00:33 +0000

    Correct

--- Beginners/01-Ruby/03-Hash-Symbols/02-Argument-order-dependency/lib/refrain.rb
+++ Beginners/01-Ruby/03-Hash-Symbols/02-Argument-order-dependency/lib/refrain.rb
@@ -10,6 +10,18 @@ def refrain(lyrics, number_of_times = 1, vibrato = 0, strong = false)
   refrain.join(" ")
 end
 
+p refrain("Lalalalalala", number_of_times = 3, vibrato = 1, strong = false)
+
 def better_refrain(lyrics, options = {vibrato: 0, number_of_times: 1})
   #TODO: implement this better version which breaks argument order dependency
+  refrain = []
+  lyrics += lyrics[lyrics.size-1] * options[:vibrato]
+  lyrics.upcase! if options[:strong]
+
+  options[:number_of_times].times do
+    refrain << lyrics
+  end
+  refrain.join(" ")
 end
+
+p better_refrain "Lalalalalalala", number_of_times: 3, vibrato: 2, strong: true
